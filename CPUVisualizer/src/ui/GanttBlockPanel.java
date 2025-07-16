816bbab39f08704c4f4ed015d3602e86cb70266d
NathalieP3ter <nathalie.peter22@gmail.com>
2025-07-16 15:01:23 +0800

    Finished typing GanttBlock
    Gantt Block code

---
+++ CPUVisualizer/src/ui/GanttBlockPanel.java
@@ -0,0 +1,10 @@
+package CPUVisualizer.src.ui;
+
+public class GanttBlockPanel{
+    public int pid, start, end;
+    public GanttBlockPanel(int pid, int start, int end){
+        this.pid = pid;
+        this.start = start;
+        this.end = end;
+   }
+}

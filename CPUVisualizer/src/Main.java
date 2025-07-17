e9ccf04830bd7f29b6dc50d581537df720b6c61c
bk-new <baneshakaur20@gmail.com>
2025-07-17 16:42:15 +0800

    Modified the main code

--- CPUVisualizer/src/Main.java
+++ CPUVisualizer/src/Main.java
@@ -4,6 +4,8 @@ import CPUVisualizer.src.algo_ui.SchedulerUI;
 
 public class Main {
     public static void main(String[] args) {
-        new SchedulerUI();
+     javax.swing.SwingUtilities.invokeLater(() -> {
+            new SchedulerUI();
+        });
     }
 }

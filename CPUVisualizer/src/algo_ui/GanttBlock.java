08cf4b9af0d33596af13548946e2a89b5767e34d
bk-new <baneshakaur20@gmail.com>
2025-07-17 16:56:43 +0800

    Added a new code for GanttBlock java

--- CPUVisualizer/src/algo_ui/GanttBlock.java
+++ CPUVisualizer/src/algo_ui/GanttBlock.java
@@ -1,2 +1,15 @@
+package CPUVisualizer.src.algo_ui;
+
+public class GanttBlock {
+    public int pid;
+    public int start;
+    public int end;
+
+    public GanttBlock(int pid, int start, int end) {
+        this.pid = pid;
+        this.start = start;
+        this.end = end;
+    }
+}
 
 

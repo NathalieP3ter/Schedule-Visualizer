019c5536d0c27cb7c18acb06b207c9abbc1d8623
NathalieP3ter <nathalie.peter22@gmail.com>
2025-07-19 12:30:53 +0800

    Testing code , no changes

--- CPUVisualizer/src/algo_ui/SchedulerLogic.java
+++ CPUVisualizer/src/algo_ui/SchedulerLogic.java
@@ -251,3 +251,4 @@ public class SchedulerLogic {
         return avg;
     }
 }
+
--- CPUVisualizer/src/algo_ui/SchedulerUI.java
+++ CPUVisualizer/src/algo_ui/SchedulerUI.java
@@ -219,3 +219,4 @@ public class SchedulerUI extends JFrame {
         SwingUtilities.invokeLater(SchedulerUI::new);
     }
 }
+ 
